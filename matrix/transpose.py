2db62a8bf821468e78652f5766c89f01b873aa8c
Francisco Cruz <157864846+francruz21@users.noreply.github.com>
2025-07-03 22:41:34 -0300

    Update transpose.py

--- matrix/transpose.py
+++ matrix/transpose.py
@@ -1,2 +1,8 @@
 def transpose(matrix):
-    pass
+    if matrix == None or len(matrix) == 0:
+        raise ValueError("no puede ser vacia")
+    aux=[[None for i in range(len(matrix))] for j in range(len(matrix[0]))]
+    for i in range(len(matrix[0])):
+        for j in range(len(matrix)):
+            aux[i][j] = matrix[j][i]
+    return aux
